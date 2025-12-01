31b5720a4b8669f71d470759b18dacd17366ec5d
Gabriel Garcia <ggarcia@coddera.com>
2025-12-01 02:29:54 -0300

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apps: [{
+    name: "hello world - 7777",
+    script: "./server.js"
+  }]
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const PORT = process.env.PORT || 7777;
+var https = require("https");
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+//SERVIDOR
+const options = {
+  key: fs.readFileSync(
+    "/home/ubuntu/certificate/genesyscloudapps.coddera.com_key.txt"
+  ),
+  cert: fs.readFileSync(
+    "/home/ubuntu/certificate/genesyscloudapps.coddera.com.crt"
+  ),
+  ca: [
+    fs.readFileSync(
+      "/home/ubuntu/certificate/genesyscloudapps.coddera.com.ca-bundle"
+    ),
+  ],
+};
+
+//RODAR O PROJETO COM HTTPS
+https.createServer(options, app).listen(PORT, function () {
+  console.log("Server HTTPS Listen port: " + PORT);
+});
