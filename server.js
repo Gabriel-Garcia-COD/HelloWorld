f9e777cbca50fa42746e30b2e527ca2efba67f73
Gabriel Garcia <ggarcia@coddera.com>
2025-12-01 09:07:37 -0300

    change https

--- server.js
+++ server.js
@@ -8,26 +8,26 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
 
 //SERVIDOR
-// const options = {
-//   key: fs.readFileSync(
-//     "/home/ubuntu/certificate/genesyscloudapps.coddera.com_key.txt"
-//   ),
-//   cert: fs.readFileSync(
-//     "/home/ubuntu/certificate/genesyscloudapps.coddera.com.crt"
-//   ),
-//   ca: [
-//     fs.readFileSync(
-//       "/home/ubuntu/certificate/genesyscloudapps.coddera.com.ca-bundle"
-//     ),
-//   ],
-// };
+const options = {
+  key: fs.readFileSync(
+    "/home/ubuntu/certificate/genesyscloudapps.coddera.com_key.txt"
+  ),
+  cert: fs.readFileSync(
+    "/home/ubuntu/certificate/genesyscloudapps.coddera.com.crt"
+  ),
+  ca: [
+    fs.readFileSync(
+      "/home/ubuntu/certificate/genesyscloudapps.coddera.com.ca-bundle"
+    ),
+  ],
+};
 
-// //RODAR O PROJETO COM HTTPS
-// https.createServer(options, app).listen(PORT, function () {
-//   console.log("Server HTTPS Listen port: " + PORT);
-// });
+//RODAR O PROJETO COM HTTPS
+https.createServer(options, app).listen(PORT, function () {
+  console.log("Server HTTPS Listen port: " + PORT);
+});
